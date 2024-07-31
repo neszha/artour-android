6cb59ed04b31d2686a6391c6cc7e4be224afdfbb
Fanesa Hadi Pramana <fanesahadi@gmail.com>
2024-07-31 07:41:49 +0700

    feat(config): update version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         minSdk = 26
         //noinspection OldTargetApi
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
